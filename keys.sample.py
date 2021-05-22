3714b3f013703e7703ea8e6ffb6629cbcceaaddc
yasin javar <yasinjjjv@gmail.com>
2021-05-22 10:54:18 +0430

    add keys.py

---
+++ keys.sample.py
@@ -0,0 +1,4 @@
+CONSUMER_KEY = "xxxxxxxxxxxxxxxxxxxxxxxxxxxx"
+CONSUMER_SECRET = "xxxxxxxxxxxxxxxxxxxxxxxxxxxxx"
+ACCESS_TOKEN = "xxxxxxxxxxxxxxxxxxxxxxxx"
+ACCESS_TOKEN_SECRET = "xxxxxxxxxxxxxxxxxxxxxxxxxxx"
